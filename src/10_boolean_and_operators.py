73308c5047fd07d550a5e8dcae12547eee79d242
nimo-ubuntu <nimodbdev@gmail.com>
2025-07-09 16:48:02 +0200

    Add Chapter 7 notes on Boolean and Operators with docs/10_boolean_and_operators.md and src/10_boolean_and_operators.py; update README.

---
+++ src/10_boolean_and_operators.py
@@ -0,0 +1,12 @@
+# Comparison Operators
+print(1 == 2)  # Check if 1 equals 2
+print(1 != 2)  # Check if 1 is not equal to 2
+print(1 > 2)   # Check if 1 is greater than 2
+print(1 < 2)   # Check if 1 is less than 2
+print(1 >= 1)  # Check if 1 is greater than or equal to 1
+print(1 <= 2)  # Check if 1 is less than or equal to 2
+
+# Logical Operators
+print(1 > 3 and 5 < 7)  # Check if 1 > 3 and 5 < 7
+print(1 > 3 or 5 < 7)   # Check if 1 > 3 or 5 < 7
+print(not(1 == 1))      # Reverse the result of 1 == 1
